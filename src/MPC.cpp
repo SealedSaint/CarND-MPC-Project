65a531a48388017de8e3ed48fca817d42a6f2e6c
David Clark <davidclark314@gmail.com>
2017-12-17 13:20:16 -0700

    Another working solution (30mph).

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -21,7 +21,7 @@ double dt = 0.1;
 const double Lf = 2.67;
 
 // Note: feel free to play around with this or do something completely different
-double ref_v = 20;
+double ref_v = 30;
 
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should establish
